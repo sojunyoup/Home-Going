e737a0be31d51695c0264318601ed0ffd2f540ec
sojunyoup <thwnsuq46@naver.com>
2021-05-14 08:54:08 +0900

    UI

---
+++ Assets/Script/3. Ingame/UiManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    //Stop UI
+    [Header("Stop UI")]
+    public Image Stop;
+    bool isStop = false;
+    [Space(10f)]
+    public Image Sound;
+    public Sprite SoundOn;
+    public Sprite SoundOff;
+    bool isSound = true;
+    [Space(10f)]
+    public Image SoundE;
+    public Sprite SoundEOn;
+    public Sprite SoundEOff;
+    bool isSoundE = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //일시정지 ui
+    public void OnClickSound()
+    {
+        isSound = !isSound;
+        if (isSound) Sound.sprite = SoundOn;
+        else Sound.sprite = SoundOff;
+    }
+    public void OnClickSoundEffect()
+    {
+        isSoundE = !isSoundE;
+        if (isSoundE) SoundE.sprite = SoundEOn;
+        else SoundE.sprite = SoundEOff;
+    }
+    public void OnClickStop()
+    {
+        isStop = !isStop;
+        Stop.gameObject.SetActive(isStop);
+    }
+    public void OnClickShop()
+    {
+        SceneManager.LoadScene("2. Shop");
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+   
+
+    private static GameManager instance = null;
+
+  
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+   
+
+}
